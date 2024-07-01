75f586c469047be47dbaa79aed3075f290d193ca
lourencomonteiro <lo.montenegro2002@gmail.com>
2024-07-01 15:45:06 -0300

    tests: adding cypress covarage

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       require('@cypress/code-coverage/task')(on, config);
       return config;
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
   },
 });
