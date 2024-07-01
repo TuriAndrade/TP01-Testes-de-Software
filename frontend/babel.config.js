66516d4a2e62afbff6ada19bb27badefb6b63ae4
lourencomonteiro <lo.montenegro2002@gmail.com>
2024-07-01 15:35:57 -0300

    tests: adding cypress covarage

---
+++ frontend/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    // other presets
+  ],
+  plugins: [
+    'istanbul',
+    // other plugins
+  ],
+};
--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,8 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
-  }
-})
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+  },
+});
